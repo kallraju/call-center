e20e0d502369e39872092f8f7e595d40a33179ac
Dinesh <dineshkumar@rbtekspace.com>
2018-08-28 11:36:24 +0530

    Add-Agent navigation
    Add-Agent navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddAgentComponent } from './agent/add-agent/add-agent.component';
 
 
 const routes: Routes = [
@@ -7,12 +8,16 @@ const routes: Routes = [
   {path: 'agent', loadChildren: 'src/app/agent/agent-routing.module#AgentRoutingModule'},
   {path: 'queue', loadChildren: 'src/app/queue/queue-routing.module#QueueRoutingModule'},
   {path: 'agent-queue', loadChildren: 'src/app/agent-queue/agent-queue-routing.module#AgentQueueRoutingModule'},
-  {path: 'mis', loadChildren: 'src/app/mis/mis-routing.module#MISRoutingModule'}
-
+  {path: 'mis', loadChildren: 'src/app/mis/mis-routing.module#MISRoutingModule'},
+  {path: 'AddAgent',
+  //loadChildren: 'src/app/admin/admin-routing.module#AdminRoutingModule',
+  component: AddAgentComponent}
+  //outlet: 'popup'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
